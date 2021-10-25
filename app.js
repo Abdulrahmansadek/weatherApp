32b79e075f67d51c3f6fce5d454bc911733347d6
Abdulrahmansadek <boody.sad@gmail.com>
2021-10-26 00:53:52 +0200

    create location and time

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
-const todayDegree = document.querySelectorAll(".hourlyDegree");
-const degree = document.querySelector(".todayDegree");
-
-todayDegree.forEach((w) => {
-  w.innerHTML = `   
+import {
+  degree,
+  todayDegree,
+  week,
+  days,
+  apiKey,
+  imgWeather,
+} from "./constants.js";
+import { weekDays } from "./data.js";
+import { update } from "./updateTimeAndDate.js";
+import { showWeather } from "./showWeather.js";
+todayDegree.forEach((today) => {
+  today.innerHTML = `   
     <span id="time">12:00</span>
-    <span id="weatherDegree"><img src="sun.png" alt="" /></span>
+    <span id="weatherDegree"><img src="http://openweathermap.org/img/wn/10d@2x.png" alt="" /></span>
     <span id="degreeDay">20</span>
   `;
 });
+
+function getWeather() {
+  navigator.geolocation.getCurrentPosition((position) => {
+    const { latitude, longitude } = position.coords;
+    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${apiKey}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        showWeather(data);
+      });
+  });
+}
+getWeather();
