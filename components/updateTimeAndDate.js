a5068cdaaaa4ac605111a5622dfabaa7ac02d7e1
Abdulrahmansadek <boody.sad@gmail.com>
2021-12-01 16:32:07 +0100

    add components file

---
+++ components/updateTimeAndDate.js
@@ -0,0 +1,15 @@
+import { dateAndTime } from "../constants.js";
+import { months, weekDays } from "../assets/data.js";
+
+const time = new Date();
+const month = time.getMonth();
+const day = time.getDay();
+export const hour = time.getHours();
+const minutes = time.getMinutes();
+export const update = setInterval(() => {
+  if (minutes < 10) {
+    dateAndTime.innerHTML = `<p>${weekDays[day]} , ${month} ${months[month]} , ${hour}:0${minutes}</p>`;
+  } else {
+    dateAndTime.innerHTML = `<p>${weekDays[day]} , ${month} ${months[month]} , ${hour}:${minutes}</p>`;
+  }
+}, 1000);
