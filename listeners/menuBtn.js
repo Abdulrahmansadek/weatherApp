fff51d55d93f9cd075ab0dc301a053eb15eab008
Abdulrahmansadek <boody.sad@gmail.com>
2021-12-05 13:27:28 +0100

    separate the menu page

--- listeners/menuBtn.js
+++ listeners/menuBtn.js
@@ -1,17 +1,12 @@
 import { container } from "../utils/constants.js";
-
+import { page } from "../views/menuPage.js";
 const menuPage = document.createElement("div");
 
 export const menuBar = () => {
   container.appendChild(menuPage);
   menuPage.className = "menu-page";
   menuPage.setAttribute("data-aos", "fade-down");
-  menuPage.innerHTML = String.raw`
- <div class="search-bar">
-   <input type="text" placeholder="city">  
-   <div> <button class="close-btn"> ← </button></div>
-  </div>
-  `;
+  menuPage.innerHTML = page;
   const closeBtn = document.querySelector(".close-btn");
   closeBtn.addEventListener("click", () => {
     menuPage.classList.add("close");
---
+++ views/menuPage.js
@@ -0,0 +1,6 @@
+export const page = String.raw`
+<div class="search-bar">
+  <input type="text" placeholder="city">  
+  <div> <button class="close-btn"> ← </button></div>
+ </div>
+ `;
