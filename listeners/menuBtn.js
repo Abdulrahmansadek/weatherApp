565a6a89fb5208aa92e5e61e81e32e93b9b4a52f
Abdulrahmansadek <boody.sad@gmail.com>
2021-12-05 13:17:55 +0100

    add closing botton and menu page

--- listeners/menuBtn.js
+++ listeners/menuBtn.js
@@ -1,5 +1,19 @@
 import { container } from "../utils/constants.js";
 
-export const menuBar = (event) => {
-  console.log(event.target);
+const menuPage = document.createElement("div");
+
+export const menuBar = () => {
+  container.appendChild(menuPage);
+  menuPage.className = "menu-page";
+  menuPage.setAttribute("data-aos", "fade-down");
+  menuPage.innerHTML = String.raw`
+ <div class="search-bar">
+   <input type="text" placeholder="city">  
+   <div> <button class="close-btn"> ← </button></div>
+  </div>
+  `;
+  const closeBtn = document.querySelector(".close-btn");
+  closeBtn.addEventListener("click", () => {
+    menuPage.classList.add("close");
+  });
 };
