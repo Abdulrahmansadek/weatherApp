203614eb4066e8d724ccf48f896be566e2f0ce1d
Abdulrahmansadek <boody.sad@gmail.com>
2021-12-07 12:42:08 +0100

    fixing bug

--- views/errorPage.js
+++ views/errorPage.js
@@ -1,6 +1,7 @@
 export const errorPage = (err) => {
   const page = document.body;
   page.innerHTML = "";
+  console.log(err);
 
   page.innerHTML = `<h1 id='error'> 404 error</h1>`;
   page.style.backgroundColor = "white";
